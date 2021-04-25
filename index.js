fd18e5dc64312a89128c1ed8fc44c9aaea652f55
Noor Abdikadir <nooromar305@gmail.com>
2021-04-24 18:08:31 -0700

    updated 2.0

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const render = require("./lib/renderHTML");
-const outputDir = path.resolve(__dirname, "output");
-const outputPath = path.join(outputDir, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const team = [];
 
